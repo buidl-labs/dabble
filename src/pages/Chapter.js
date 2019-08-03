39e2e3aa0f3c026f04bb50229d9c0db9ae98ec30
Prastut <kr.prastut@gmail.com>
2019-08-03 15:19:53 +0530

    Completed Reading Module

--- src/pages/Chapter.js
+++ src/pages/Chapter.js
@@ -11,14 +11,103 @@ import { Typography, Divider } from "antd";
 import { Card } from "antd";
 import { Button } from "antd";
 
+//Internal Components
 import { sampleContent } from "../utils/utils";
+import EnterStakingAmount from "../components/EnterStakingAmount";
+import ConfirmStakeModel from "../components/ConfirmStakeModel";
 
 const { Paragraph, Title, Text } = Typography;
 const { Countdown } = Statistic;
 const { Header, Content, Footer, Sider } = Layout;
 const ButtonGroup = Button.Group;
 
-const StakingPanel = ({ question, stakedOn, updateStakeStatus }) => {
+const MarketResolvedPanel = ({ question, resolutionDetails }) => (
+  <Row gutter={16}>
+    <Col span={24} style={{ marginTop: 24, textAlign: "center" }}>
+      <Typography>
+        <Title level={4}>{question} in the next chapter?</Title>
+      </Typography>
+    </Col>
+    <Col span={12} style={{ marginTop: 24, textAlign: "center" }}>
+      <Statistic
+        title="Your Vote"
+        value={resolutionDetails.yourVote ? "Yes" : "No"}
+      />
+    </Col>
+    <Col span={12} style={{ marginTop: 24, textAlign: "center" }}>
+      <Statistic
+        title="Community Vote"
+        value={resolutionDetails.communityVote ? "Yes" : "No"}
+      />
+    </Col>
+    <Col span={12} style={{ marginTop: 24, textAlign: "center" }}>
+      <Statistic
+        title="Youe Winnings"
+        value={`$${resolutionDetails.yourWinnings}`}
+      />
+    </Col>
+    <Col span={12} style={{ marginTop: 24, textAlign: "center" }}>
+      <Statistic
+        title="Community Winnings"
+        value={`$${resolutionDetails.communityWinnings}`}
+      />
+    </Col>
+  </Row>
+);
+
+const StakingInterface = ({
+  stakedOn,
+  updateStakeStatus,
+  updateStakingAmount,
+  stakedAmount,
+  updateReaderIncentivizationProcessStatus
+}) => (
+  <>
+    <Col span={24} style={{ marginTop: 24, textAlign: "center" }}>
+      <ButtonGroup>
+        <Button
+          type={stakedOn ? "primary" : ""}
+          onClick={() => updateStakeStatus(true)}
+        >
+          Yes
+        </Button>
+        <Button
+          type={stakedOn ? "" : "primary"}
+          onClick={() => updateStakeStatus(false)}
+        >
+          No
+        </Button>
+      </ButtonGroup>
+    </Col>
+    <Col span={24} style={{ marginTop: 24, textAlign: "center" }}>
+      <EnterStakingAmount
+        updateStakingAmount={updateStakingAmount}
+        stakedAmount={stakedAmount}
+      />
+    </Col>
+    <Col span={24} style={{ marginTop: 24, textAlign: "center" }}>
+      <ConfirmStakeModel
+        stakedOn={stakedOn}
+        stakedAmount={stakedAmount}
+        updateReaderIncentivizationProcessStatus={
+          updateReaderIncentivizationProcessStatus
+        }
+      />
+    </Col>
+  </>
+);
+
+const StakingFinishedInterace = () => <h1>Thank you for voting!</h1>;
+
+const StakingPanel = ({
+  question,
+  stakedOn,
+  updateStakeStatus,
+  updateStakingAmount,
+  stakedAmount,
+  isReaderIncentivizationProcessFinished,
+  updateReaderIncentivizationProcessStatus
+}) => {
   return (
     <>
       <Col span={24}>
@@ -31,22 +120,19 @@ const StakingPanel = ({ question, stakedOn, updateStakeStatus }) => {
           <Title level={4}>{question} in the next chapter?</Title>
         </Typography>
       </Col>
-      <Col span={24} style={{ marginTop: 24, textAlign: "center" }}>
-        <ButtonGroup>
-          <Button
-            type={stakedOn ? "primary" : ""}
-            onClick={() => updateStakeStatus(true)}
-          >
-            Yes
-          </Button>
-          <Button
-            type={stakedOn ? "" : "primary"}
-            onClick={() => updateStakeStatus(false)}
-          >
-            No
-          </Button>
-        </ButtonGroup>
-      </Col>
+      {isReaderIncentivizationProcessFinished ? (
+        <StakingFinishedInterace />
+      ) : (
+        <StakingInterface
+          stakedOn={stakedOn}
+          updateStakeStatus={updateStakeStatus}
+          updateStakingAmount={updateStakingAmount}
+          stakedAmount={stakedAmount}
+          updateReaderIncentivizationProcessStatus={
+            updateReaderIncentivizationProcessStatus
+          }
+        />
+      )}
     </>
   );
 };
@@ -77,7 +163,11 @@ const IncentivizeReadersPanel = ({
   updateReadStatus,
   question,
   stakedOn,
-  updateStakeStatus
+  updateStakeStatus,
+  stakedAmount,
+  updateStakingAmount,
+  isReaderIncentivizationProcessFinished,
+  updateReaderIncentivizationProcessStatus
 }) => {
   const deadline = Date.now() + 1000 * 60 * 60 * 24 * 2 + 1000 * 30; // Moment is also OK
 
@@ -86,6 +176,14 @@ const IncentivizeReadersPanel = ({
       question={question}
       stakedOn={stakedOn}
       updateStakeStatus={updateStakeStatus}
+      stakedAmount={stakedAmount}
+      updateStakingAmount={updateStakingAmount}
+      isReaderIncentivizationProcessFinished={
+        isReaderIncentivizationProcessFinished
+      }
+      updateReaderIncentivizationProcessStatus={
+        updateReaderIncentivizationProcessStatus
+      }
     />
   ) : (
     <UpdateReadingPanel updateReadStatus={updateReadStatus} />
@@ -111,7 +209,10 @@ class Book extends Component {
     read: false,
     predictionMarket: false,
     question: "",
-    stakedOn: true
+    stakedOn: true,
+    stakingAmount: "",
+    isReaderIncentivizationProcessFinished: false,
+    resolutionDetails: {}
   };
 
   componentDidMount() {
@@ -119,19 +220,39 @@ class Book extends Component {
 
     const chapterId = this.props.match.params.id;
 
+    const predictionMarket = false;
+
     //From chapter ID fetch book details
     const chapterDetails = {
       bookTitle: "Harry Potter",
       chapterId,
       content: sampleContent,
-      predictionMarket: true,
+      predictionMarket,
       question: "Should I kill Harry Potter"
     };
+
+    let resolutionDetails;
+    if (predictionMarket === false) {
+      //Fetch Resolution Panel Details
+      //Fetch your vote
+      //fetch community vote
+      //fetch your winnings
+      //fetch community winnings
+      resolutionDetails = {
+        yourVote: true,
+        communityVote: false,
+        yourWinnings: -5,
+        communityWinnings: 33
+      };
+    }
 
     // console.log(sampleContent);
     //from chapter Id fetch chapter details
 
-    this.setState({ ...chapterDetails });
+    this.setState({
+      ...chapterDetails,
+      resolutionDetails
+    });
   }
 
   goBack = () => {
@@ -144,6 +265,15 @@ class Book extends Component {
 
   updateStakeStatus = clickedOn => {
     this.setState({ stakedOn: clickedOn });
+  };
+
+  updateStakingAmount = amount => {
+    console.log(amount);
+    this.setState({ stakingAmount: amount });
+  };
+
+  updateReaderIncentivizationProcessStatus = status => {
+    this.setState({ isReaderIncentivizationProcessFinished: status });
   };
 
   render() {
@@ -177,45 +307,22 @@ class Book extends Component {
             updateReadStatus={this.updateReadStatus}
             question={this.state.question}
             stakedOn={this.state.stakedOn}
-            updateStakeStatus={this.updateStakeStatus}
+            updateStakeStatus={this.updateStakeStatus} // Voted on Yes or No
+            stakedAmount={this.state.stakingAmount}
+            updateStakingAmount={this.updateStakingAmount}
+            isReaderIncentivizationProcessFinished={
+              this.state.isReaderIncentivizationProcessFinished
+            } //Has the user voted or not
+            updateReaderIncentivizationProcessStatus={
+              this.updateReaderIncentivizationProcessStatus
+            }
           />
         ) : (
-          <h1> Resolution Panel </h1>
+          <MarketResolvedPanel
+            question={this.state.question}
+            resolutionDetails={this.state.resolutionDetails}
+          />
         )}
-
-        {/* <Typography>
-          <Paragraph>{this.state.content}</Paragraph>
-        </Typography> */}
-        {/* <List
-          itemLayout="vertical"
-          dataSource={this.state.chapters}
-          renderItem={item => (
-            <List.Item style={{ marginLeft: 50, marginRight: 50 }}>
-              <List.Item.Meta
-                title={<a href="/">Chapter {item.id}</a>}
-                // description="Ant Design, a design language for background applications, is refined by Ant UED Team"
-              />
-              <Row gutter={16}>
-                <Col span={8}>
-                  <Statistic
-                    title="Prediction Market"
-                    value={item.predictionMarket ? "Live" : "Passed"}
-                  />
-                </Col>
-                <Col span={8}>
-                  <Statistic title="Staked" value={`$${item.staked}`} />
-                </Col>
-                <Col span={8}>
-                  {item.predictionMarket ? (
-                    <Countdown title="Countdown" value={deadline} />
-                  ) : (
-                    <Statistic title="Countdown" value={0} />
-                  )}
-                </Col>
-              </Row>
-            </List.Item>
-          )}
-        /> */}
       </Layout>
     );
   }
